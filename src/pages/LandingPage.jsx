e349bb973284842dccf8f171649e0557d25746cd
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:59:16 -0400

    Created LandingPage.jsx

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LandingPage = () => {
+
+
+  return (
+    <div className="">
+
+
+
+    </div>
+  );
+};
+
+export default LandingPage;
