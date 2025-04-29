cf56700af52574cc7d92448594b0f3869ec6d84d
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:42:10 -0400

    Created AboutUs.jsx setup

---
+++ src/components/CommonComponents/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Linkedin } from 'lucide-react';
+import { FiGithub } from 'react-icons/fi';
+import { Mail } from 'lucide-react';
+
+function AboutUs() {
+  return (
+    <div className="bg-gradient-to-b from-light-blue to-white min-h-screen flex flex-col items-center py-16 px-6">
+
+      {/* Header */}
+      <div className="text-center mb-12">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default AboutUs;
