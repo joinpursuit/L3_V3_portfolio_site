d4f9f9fb384192c098558e4c275c1b55f50ae747
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:51:27 -0400

    Updated NavBar.jsx with functions

--- src/components/CommonComponents/Navbar.jsx
+++ src/components/CommonComponents/Navbar.jsx
@@ -4,7 +4,32 @@ import { useNavigate } from 'react-router-dom';
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
+  const handleNavigateLanding = () => {
+    navigate('/');
+    setIsOpen(false);
+  };
+
+  const handleNavigateAbout = () => {
+    navigate('/about');
+    setIsOpen(false);
+  };
+
+  const handleNavigateProjects = () => {
+    navigate('/projects');
+    setIsOpen(false);
+  };
+
+  const handleNavigateContact = () => {
+    navigate('/contact');
+    setIsOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen); 
+  };
 
   return (
     <header className="bg-gray-50 overflow-hidden z-10 relative border-b-4 border-gray-500 shadow-dark-lg">
