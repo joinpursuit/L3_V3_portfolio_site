ee52c30ae1ab9ffe6967fea1896e811ef1445849
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 21:02:31 -0400

    Created project list mock data will need to be updated

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Projects = () => {
-
+  const projectList = [
+    {
+      title: 'Project One',
+      description: 'This is a sample description for Project One.',
+      link: '#',
+    },
+    {
+      title: 'Project Two',
+      description: 'This is a sample description for Project Two.',
+      link: '#',
+    }
+  ];
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-light-blue flex flex-col items-center py-10">
