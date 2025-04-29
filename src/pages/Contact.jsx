63274ac0b6eb5060b1b262088f7fbca70d20add9
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:56:52 -0400

    Created Contact.jsx page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-white to-light-blue flex flex-col items-center justify-center py-10">
+
+    </div>
+  );
+};
+
+export default Contact;
