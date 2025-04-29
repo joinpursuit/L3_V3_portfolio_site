0bfe9a9e069a88376ac7fd47d0c5dc548faf78b4
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 21:02:00 -0400

    Created  Projects.jsx page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+
+
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-white to-light-blue flex flex-col items-center py-10">
+
+    </div>
+  );
+};
+
+export default Projects;
