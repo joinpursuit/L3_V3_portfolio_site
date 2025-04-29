5b69fbc1de12fb33787f60668a8ec2f510edb2c9
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 21:00:10 -0400

    Created nagigate functions and minor tailwind

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+  const navigate = useNavigate();
 
+  const handleContactClick = () => {
+    navigate('/contact');
+  };
+
+  const handleProjectsClick = () => {
+    navigate('/projects');
+  };
 
   return (
-    <div className="">
+    <div className="min-h-screen bg-gradient-to-b from-light-blue to-white flex flex-col items-center">
 
 
 
