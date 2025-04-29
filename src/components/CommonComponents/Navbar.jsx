1e5f4e4b241da396776a9bd29221fc7b5fcbc4c1
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:50:54 -0400

    Created Navbar and set up basic imports for future hamburger menu

---
+++ src/components/CommonComponents/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import "./Navbar.css";
+
+const Navbar = () => {
+
+
+  return (
+    <header className="bg-gray-50 overflow-hidden z-10 relative border-b-4 border-gray-500 shadow-dark-lg">
+
+    </header>
+  );
+};
+
+export default Navbar;
