4022ede401994ee7170ad0587f0661d216e1450e
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:47:38 -0400

    Created Footer.jsx basic footer

---
+++ src/components/CommonComponents/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-light-blue text-white py-6">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
