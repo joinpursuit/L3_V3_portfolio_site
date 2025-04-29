0af6b5e7c37f49bdac336713f5779b54f0bcb184
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-04-28 20:48:21 -0400

    Updated Footer.jsx with tailwind and text

--- src/components/CommonComponents/Footer.jsx
+++ src/components/CommonComponents/Footer.jsx
@@ -4,6 +4,17 @@ const Footer = () => {
   return (
     <footer className="bg-light-blue text-white py-6">
       <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+        
+        <p className="text-center text-sm md:text-base mb-4 md:mb-0">
+          © {new Date().getFullYear()} Michael Restrepoross | All Rights Reserved
+        </p>
+
+        <div className="flex space-x-6">
+          <a href="/" className="hover:underline hover:text-duke-blue transition duration-300">Home</a>
+          <a href="/about" className="hover:underline hover:text-duke-blue transition duration-300">About</a>
+          <a href="/projects" className="hover:underline hover:text-duke-blue transition duration-300">Projects</a>
+          <a href="/contact" className="hover:underline hover:text-duke-blue transition duration-300">Contact</a>
+        </div>
 
       </div>
     </footer>
